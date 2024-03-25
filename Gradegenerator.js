89d307afad57a89e1d5b2d2614c71d2dacf78c1d
Benhin <benhinmwendwa7@gmail.com>
2024-03-26 00:14:50 +0300

    challenge

--- Gradegenerator.js
+++ Gradegenerator.js
@@ -10,10 +10,10 @@ if (grade>79 && grade <=100){ //if  marks is above 79 then you will be graded A
 }else if  (grade >= 60 && grade<= 79 ){//if marks is greater or equal to 60 and is less or equal to 79 then you will be graded a B
     return "B"
 
-}else if  (grade>= 49 && grade<= 59 ){//if marks is greater or equal to 49 and is less or equal to 59 then you will be graded a C
+}else if  (grade>49 && grade<= 59 ){//if marks is greater or equal to 49 and is less or equal to 59 then you will be graded a C
     return "C"
 }
-else if  (grade>=40 && grade<= 48.9 ){//if marks is greater or equal to 40 and is less or equal to 48.9then you will be graded a D
+else if  (grade>=40 && grade<= 49 ){//if marks is greater or equal to 40 and is less or equal to 48.9then you will be graded a D
     return "D"
 }
 else if  ( grade< 40 ){//if marks  is less than 40 then you will be graded a E
